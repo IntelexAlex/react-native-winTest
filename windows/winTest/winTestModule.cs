3704cc34cc3562b205c3669608b06a2ac27bb6c1
Alex Wade <alex.wade@intelex.com>
2016-11-25 13:42:38 -0800

    initial commit

---
+++ windows/winTest/winTestModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactNative.Bridge;
+using ReactNative.Collections;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+
+namespace test.intelexalex.winTest
+{
+    class winTestModule : ReactContextNativeModuleBase
+    {
+        private const String testMessage = "This is a test message";
+
+        public winTestModule(ReactContext reactContext) : base(reactContext){}
+
+        public override string Name
+        {
+            get
+            {
+                return "winTest";
+            }
+        }
+
+        [ReactMethod]
+        public void getMessage(ICallback callback)
+        {
+            callback.Invoke(testMessage);
+        }
+    }
+}
---
+++ windows/winTest/winTestPackage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace test.intelexalex.winTest
+{
+    class winTestPackage : IReactPackage
+    {
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>
+            {
+                new winTestModule(reactContext)
+            };
+        }
+
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
+        {
+            return Array.Empty<Type>();
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers(ReactContext reactContext)
+        {
+            return new List<IViewManager>(0);
+        }
+    }
+}
